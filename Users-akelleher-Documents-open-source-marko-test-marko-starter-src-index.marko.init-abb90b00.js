44ba63ba144bf02b9315c31bc923df327d723ebe
Austin Kelleher <austin.kell47@gmail.com>
2018-01-26 14:36:00 -0500

    Updates

--- Users-akelleher-Documents-open-source-marko-test-marko-starter-src-index.marko.init-abb90b00.js
+++
@@ -1,2 +0,0 @@
-$_mod.def("/marko-starter-demo$1.0.9/src/index.marko.init",function(n,o,i,t,d){window.$initComponents&&window.$initComponents()});
-$_mod.run("/marko-starter-demo$1.0.9/src/index.marko.init");
